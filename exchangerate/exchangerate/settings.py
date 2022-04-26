cba5c05edc3f91a99c3cfabe43ea2c01c33afdf5
ptrick0 <ptrick-13@hotmail.com>
2022-04-26 11:33:10 -0300

    modified time zone

--- exchangerate/exchangerate/settings.py
+++ exchangerate/exchangerate/settings.py
@@ -103,9 +103,15 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'pt-br'
+LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'America/Sao_Paulo'
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
